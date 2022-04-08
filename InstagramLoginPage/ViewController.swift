8040a5a78999a9f9eb414397a2a43b5750dbfc60
Samandarbek <samandartoshpulatov0704gmail.com>
2022-04-08 08:30:52 +0500

    Initial Commit

---
+++ InstagramLoginPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramLoginPage
+//
+//  Created by Macservis on 08/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
